5696ab18d75d651a4c28058d40599add44e64748
Srdjan <srdjandrakul@gmail.com>
2019-08-10 18:35:31 +0200

    issues with nginx fixed

--- vagrant/configure-symlinks.sh
+++ vagrant/configure-symlinks.sh
@@ -4,7 +4,3 @@ sudo rm -rf /etc/nginx/sites-available
 sudo ln -sf /var/www/vagrant/nginx /etc/nginx/sites-enabled
 sudo ln -sf /var/www/vagrant/nginx /etc/nginx/sites-available
 sudo service nginx restart
-
-echo "-- Symlinking MOTD"
-unlink /etc/motd
-ln -sf /var/www/vagrant/motd.txt /etc/motd
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,13 +1,13 @@
 # Install php and other packages needed
-sudo apt install software-properties-common python-software-properties -y
+sudo apt install software-properties-common -y
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 sudo apt install php7.3 php7.3-cli php7.3-common php7.3-mbstring php7.3-intl  -y
 sudo apt install curl git unzip -y
 
 # Remove apache packages and install nginx
-sudo apt purge apache2 apache2-data apache2-utils -y
-sudo apt install nginx
+sudo apt purge  apache2 apache2-bin apache2-data apache2-utils -y
+sudo apt install nginx -y
 
 # Install Composer
 sudo curl -s https://getcomposer.org/installer | php
@@ -19,7 +19,6 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs
 
 sudo apt autoremove -y
-
 # Run Composer
 cd /var/www
 sudo -Hu vagrant composer install
